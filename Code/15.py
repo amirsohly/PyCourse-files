14ee1e19fcab79c039d8ffb032005e63ef405276
Amirreza Soheiliarasi <soheilyamirreza@yahoo.com>
2025-07-12 17:17:43 +0330

    Add files via upload

---
+++ Code/15.py
@@ -0,0 +1,14 @@
+Amirreza = {
+    "fName":"Amirreza",
+    "lName":"Soheili",
+    "Id":"123456789"
+}
+Amirreza.pop("lName")
+Amirreza["age"] = "25"
+Amirreza["Id"] = "987654321"
+
+print(Amirreza["age"])
+
+
+a = dict(name="Ali", age = 36)
+print(a)
---
+++ Code/21.py
@@ -0,0 +1,20 @@
+a=10
+b=2
+#print(pow(a,b))
+
+c=-50
+#print(abs(c))
+
+import math
+#print(math.sqrt(a))
+
+d=5.6
+#print(math.ceil(d))
+#print(math.floor(d))
+
+list = [10,20,30]
+tuple = (40,50,60)
+set = {70,80,90}
+print(max(list), min(list))
+print(max(tuple), min(tuple))
+print(max(set), min(set))
